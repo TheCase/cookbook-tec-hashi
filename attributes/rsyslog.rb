108b5355c58f80588f7d737753a195217b6d5aaf
TheCase <case@repulsor.net>
2018-01-18 15:05:08 -0700

    typo

--- attributes/rsyslog.rb
+++ attributes/rsyslog.rb
@@ -1,3 +1,3 @@
 default['rsyslog']['port'] = '5514;ls_json'
-default['rsyslog']['protocol'] 'tcp'
+default['rsyslog']['protocol'] = 'tcp'
 default['rsyslog']['templates'] = [ "ls_json,\"{%timestamp:::date-rfc3339,jsonf:timestamp%,%source:::jsonf:@source_host%,\\\"@source\\\":\\\"syslog://%fromhost-ip:::json%\\\",\\\"@message\\\":\\\"%msg:::json%\\\",\\\"facility\\\":\\\"%syslogfacility-text:::json%\\\",\\\"severity\\\":\\\"%syslogseverity-text:::json%\\\",\\\"program\\\":\\\"%app-name:::json%\\\",\\\"procid\\\":\\\"%procid:::json%\\\"}\"" ]   
